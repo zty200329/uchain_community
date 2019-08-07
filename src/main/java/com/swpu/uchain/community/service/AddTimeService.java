9dd58a1f2efef2994e1d3bd141ad0761a6fa73f8
Darrick <904516865@qq.com>
2019-08-07 13:31:24 +0800

    Apply addTime service

---
+++ src/main/java/com/swpu/uchain/community/service/AddTimeService.java
@@ -0,0 +1,33 @@
+package com.swpu.uchain.community.service;
+
+import com.swpu.uchain.community.dto.AddInfoDTO;
+import com.swpu.uchain.community.dto.AddTimeDTO;
+import com.swpu.uchain.community.form.AddInfoForm;
+import com.swpu.uchain.community.vo.ResultVO;
+
+
+/**
+ * @author；lzh
+ * @Date:2019/8/613:15 Descirption:
+ */
+public interface AddTimeService {
+
+    /**
+     * 获取补签列表
+     * @return
+     */
+    ResultVO getAllAddInfo();
+
+    /**
+     * 创建一个补签信息
+     * @return
+     */
+    ResultVO insert(AddInfoForm addInfoForm);
+
+    /**
+     * 管理员审核补签
+     * @return
+     */
+    ResultVO check(AddTimeDTO addTimeDTO);
+
+}
---
+++ src/main/java/com/swpu/uchain/community/service/impl/AddTimeServiceImpl.java
@@ -0,0 +1,57 @@
+package com.swpu.uchain.community.service.impl;
+
+import com.swpu.uchain.community.dao.AddInfoMapper;
+import com.swpu.uchain.community.dto.AddInfoDTO;
+import com.swpu.uchain.community.dto.AddTimeDTO;
+import com.swpu.uchain.community.entity.AddInfo;
+import com.swpu.uchain.community.enums.ResultEnum;
+import com.swpu.uchain.community.exception.BasicException;
+import com.swpu.uchain.community.form.AddInfoForm;
+import com.swpu.uchain.community.service.AddTimeService;
+import com.swpu.uchain.community.util.ResultVOUtil;
+import com.swpu.uchain.community.vo.ResultVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author；lzh
+ * @Date:2019/8/613:17 Descirption:
+ */
+@Service
+public class AddTimeServiceImpl implements AddTimeService {
+
+    private final Integer INITAIL_ADDSTATE = new Integer(0);
+
+    @Autowired
+    private AddInfoMapper addInfoMapper;
+
+    @Override
+    public ResultVO getAllAddInfo() {
+        List<AddInfo> addInfoList = addInfoMapper.selectAll();
+        return ResultVOUtil.success(addInfoList);
+    }
+
+    @Override
+    public ResultVO insert(AddInfoForm addInfoForm) {
+        AddInfoDTO addInfoDTO = new AddInfoDTO();
+        BeanUtils.copyProperties(addInfoForm,addInfoDTO);
+        addInfoDTO.setAddState(INITAIL_ADDSTATE);
+        int result = addInfoMapper.insert(addInfoDTO);
+        if(result != 1){
+            throw new BasicException(ResultEnum.SQL_ERROR.getCode(),ResultEnum.SQL_ERROR.getMsg());
+        }
+        return ResultVOUtil.success(addInfoDTO);
+    }
+
+    @Override
+    public ResultVO check(AddTimeDTO addTimeDTO) {
+        int result = addInfoMapper.updateAddTimeState(addTimeDTO);
+        if(result != 1){
+            throw new BasicException(ResultEnum.SQL_ERROR.getCode(),ResultEnum.SQL_ERROR.getMsg());
+        }
+        return ResultVOUtil.success(addTimeDTO);
+    }
+}
